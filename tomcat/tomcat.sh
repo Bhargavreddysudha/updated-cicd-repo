02913145206befccbe3d76ebd3acd4d29f44d61d
Bhargavreddysudha <130338393+Bhargavreddysudha@users.noreply.github.com>
2023-05-15 12:05:29 +0530

    Update tomcat.sh

--- tomcat/tomcat.sh
+++ tomcat/tomcat.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+yum install java git -y
 cd /opt
 wget https://archive.apache.org/dist/tomcat/tomcat-10/v10.0.8/bin/apache-tomcat-10.0.8.tar.gz
 sudo tar xzvf apache-tomcat-10.0.8.tar.gz
